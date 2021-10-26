1d6e681582bfcf8ea53f58b8e431141bf8390e73
Youngking0914 <ace_lephant@naver.com>
2021-10-26 10:08:56 +0900

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,17 +1,17 @@
 import UIKit
+import SwiftSoup
+import Alamofire
 
-var addClosure: (Int, Int) -> Int = { a, b in return a + b }
-var subClosure: (Int, Int) -> Int = { a, b in return a - b }
-var mulClosure: (Int, Int) -> Int = { a, b in return a * b }
-var divClosure: (Int, Int) -> Int = { a, b in return a / b }
-
-func calculateTwoNum(a: Int, b: Int, operation: (Int, Int) -> Int) -> Int {
-    let result = operation(a, b)
-    return result
-}
-
-calculateTwoNum(a: 5, b: 4, operation: addClosure)
-calculateTwoNum(a: 4, b: 2, operation: subClosure)
-calculateTwoNum(a: 5, b: 6, operation: mulClosure)
-calculateTwoNum(a: 3, b: 2, operation: divClosure)
+let ROOT_URL = "https://www.nike.com"
     
+func request(path: String = "/kr") -> DataRequest {
+    let response = AF.request(ROOT_URL + path,
+                                method: .get,
+                                parameters: nil,
+                                encoding: URLEncoding.default,
+                                headers: ["user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.272 Whale/2.9.118.16 Safari/537.36"])
+                    .validate(statusCode: 200..<300)
+    //let soup = SwiftSoup.parse()
+    print(response)
+    return response
+}
